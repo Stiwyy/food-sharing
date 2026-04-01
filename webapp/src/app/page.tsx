c63485105522452b14f29a875ed98e5bee747ce6
Stiwyy <andrinrenggli@outlook.com>
2026-04-01 14:51:40 +0200

    create structure

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,5 +1,9 @@
-export default function Home() {
-  return (
+import AuthCard from "@/components/auth/AuthCard";
 
+export default function HomePage() {
+  return (
+      <main className="min-h-screen bg-slate-100 flex items-center justify-center p-4">
+        <AuthCard />
+      </main>
   );
 }
